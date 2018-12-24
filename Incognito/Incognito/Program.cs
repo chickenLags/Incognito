9b5ab981296bd5e6b25863442d3601a9709a73d4
roi meimon <roi meimon>
2018-12-24 22:31:52 +0100

    added empty c# solution

---
+++ Incognito/Incognito/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Incognito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // display window:
+
+            // set background to black
+
+            // show framerate
+
+
+
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Incognito
 2D / 3D game in cooperation
+
+TODO:
+	- add window (for drawing to) openGl?
+	- add input listener
+	- add gameloop
+	- add game objects
+	- add sprite object (inherit from gameobject)
+	- add world datastructure
+	- add world render 2D
+	- add world render 3D (2.5D)
+	- 
